b2f0eeff2b72074457ef4b278ccd88207bfc2e42
unknown <etienne-2.beauchamp@polymtl.ca>
2019-11-20 20:53:36 -0500

    Implemented first section; seems to work

--- TP3/main.py
+++ TP3/main.py
@@ -1,4 +1,7 @@
 from softmaxclassifier import *
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 def testOneHot():
@@ -23,5 +26,32 @@ def testOneHot():
     softmax.fit(np.zeros((3, 4)))
 
 
+def testSol():
+    # load dataset
+    data, target = load_iris().data, load_iris().target
+
+    # split data in train/test sets
+    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=42)
+
+    # standardize columns using normal distribution
+    # fit on X_train and not on X_test to avoid Data Leakage
+    s = StandardScaler()
+    X_train = s.fit_transform(X_train)
+    X_test = s.transform(X_test)
+
+    cl = SoftmaxClassifier()
+
+    # train on X_train and not on X_test to avoid overfitting
+    train_p = cl.fit_predict(X_train, y_train)
+    test_p = cl.predict(X_test)
+
+    from sklearn.metrics import precision_recall_fscore_support
+
+    # display precision, recall and f1-score on train/test set
+    print("train : " + str(precision_recall_fscore_support(y_train, train_p, average="macro")))
+    print("test : " + str(precision_recall_fscore_support(y_test, test_p, average="macro")))
+
+
 if __name__ == '__main__':
-    testOneHot()
+    # testOneHot()
+    testSol()
--- TP3/softmaxclassifier.py
+++ TP3/softmaxclassifier.py
@@ -35,31 +35,35 @@ class SoftmaxClassifier(BaseEstimator, ClassifierMixin):
         self, in sklearn the fit method returns the object itself
     """
 
+    def add_bias(self, X):
+        return np.hstack((np.ones((np.size(X, 0), 1)), X))
+
     def fit(self, X, y=None):
         prev_loss = np.inf
         self.losses_ = []
 
         m, n = X.shape
-        # k = np.max(y)  # TODO: Comment instancier le nombre de classes?
-        k = 6
-
         self.nb_feature = n
+        k = np.max(y) + 1
         self.nb_classes = k
+        y = self._one_hot(y)
 
-        bias_column = np.ones((m, 1))  # colonne est remplie de 1
-        X_bias = np.hstack((bias_column, X))
-        self.theta_ = np.random.rand(n, k)
+
+        X_bias = self.add_bias(X)
+        self.theta_ = np.random.rand(n+1, k)
 
         for epoch in range(self.n_epochs):
 
-            # logits =
-            # probabilities =
+            logits = X_bias @ self.theta_
+            probabilities = self._softmax(logits)
 
-            # loss =
-            # self.theta_ =
+            loss = self._cost_function(probabilities, y)
+            self.theta_ = self.theta_ - self.lr*self._get_gradient(X_bias, y, probabilities)
 
             if self.early_stopping:
-                pass
+                if prev_loss - loss < self.threshold:
+                    break
+                prev_loss = loss
 
         return self
 
@@ -68,8 +72,8 @@ class SoftmaxClassifier(BaseEstimator, ClassifierMixin):
             getattr(self, "theta_")
         except AttributeError:
             raise RuntimeError("You must train classifer before predicting data!")
-        X_bias = np.hstack((np.ones((np.size(X, 0), 1)), X))
-        z = X*self.theta_  # TODO: Pas certain que ce soit ce qui est demandé
+        X_bias = self.add_bias(X)
+        z = X_bias @ self.theta_  # TODO: Pas certain que ce soit ce qui est demandé
         # Retourne les probabilités associées à chaque classe pour chacune des instances à prédire
         return self._softmax(z)
 
@@ -81,28 +85,33 @@ class SoftmaxClassifier(BaseEstimator, ClassifierMixin):
         # Probabilités des différentes classes
         p_x_k = self.predict_proba(X, y)
         # Retourne l'indice (classe) avec la plus grande probabilité pour chacunes des intances à prédire.
-        return max(np.arange(len(p_x_k)), key=p_x_k.__getitem__)
+        # return max(np.arange(len(p_x_k)), key=p_x_k.__getitem__)
+        return np.argmax(p_x_k, axis=1)
 
     def fit_predict(self, X, y=None):
         self.fit(X, y)
         return self.predict(X,y)
 
     def score(self, X, y=None):
-        pass
+        return (self.predict(X, y) == y).sum() / y.shape[0]
 
     def _cost_function(self, probabilities, y):
-        pass
+        m, K = y.shape
+        # Permet de s'assurer que les prob sont comprises entre epsilon et epsilon - 1
+        np.clip(probabilities, self.eps, 1 - self.eps)
+        return -np.sum(y * np.log(probabilities))/m
 
     def _one_hot(self, y):
         y_ohe = np.zeros((len(y), self.nb_classes))
-        i = 0
-        for x_i in y:
-            y_ohe[i, x_i] = 1
-            i += 1
+        for i in range(len(y)):
+            y_ohe[i, y[i]] = 1
         return y_ohe
 
-    def _softmax(self, z):
-        return np.exp(z) / np.sum(np.exp(z))
+    def _softmax(self, z, axis=1):
+        z_p = z - np.max(z, axis=axis)[:, None]  # Pour la stabilité du softmax (translation-invariant)
+        exp = np.exp(z_p)
+        return exp / np.sum(exp, axis=axis)[:, None]
 
     def _get_gradient(self, X_bias, y, probas):
-        pass
+        m, K = y.shape
+        return X_bias.T@(probas - y)/m
